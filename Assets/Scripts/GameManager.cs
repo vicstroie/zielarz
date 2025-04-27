9694f3d4e13206d5aee1c51d21adcad3dac38294
vicstroie <vas7119@nyu.edu>
2025-04-27 15:46:23 -0400

    title screen and introsequence

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using Fungus;
+using StarterAssets;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] GameObject flowchart;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject grandma;
+    [SerializeField] Transform grandmaEndTransform;
 
     public bool isTalking;
+    bool movedGrandma;
 
 
     // Start is called before the first frame update
@@ -21,5 +26,20 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         isTalking = flowchart.GetComponent<Flowchart>().GetBooleanVariable("isTalking");
+
+        if (isTalking)
+        {
+            player.GetComponent<FirstPersonController>().enabled = false;
+        }
+        else
+        {
+            if (!player.GetComponent<FirstPersonController>().isActiveAndEnabled) player.GetComponent<FirstPersonController>().enabled = true;
+        }
+
+        if (!movedGrandma && flowchart.GetComponent<Flowchart>().GetBooleanVariable("doneTutorial"))
+        {
+            grandma.transform.position = grandmaEndTransform.position;
+            grandma.transform.rotation = grandmaEndTransform.rotation;
+        }
     }
 }
